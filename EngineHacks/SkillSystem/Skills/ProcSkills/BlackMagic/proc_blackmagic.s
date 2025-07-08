cc6b4c5ae630bfb1d92392d972ca10f2a421efb4
blademastersf <103551595+blademastersf@users.noreply.github.com>
2025-07-07 20:58:22 -0400

    Poison point

--- EngineHacks/SkillSystem/Skills/ProcSkills/BlackMagic/proc_blackmagic.s
+++ EngineHacks/SkillSystem/Skills/ProcSkills/BlackMagic/proc_blackmagic.s
@@ -38,12 +38,12 @@ mov		r14,r3
 .short	0xF800
 cmp		r0,#0
 beq		End
-ldrb	r0,[r4,#0x15]	@skill% proc rate
+@ldrb	r0,[r4,#0x15]	@skill% proc rate
 @mov		r0,#100			@for testing
-mov		r1,r4
-blh		d100Result
-cmp		r0,#1
-bne		End				@didn't proc
+@mov		r1,r4
+@blh		d100Result
+@cmp		r0,#1
+@bne		End				@didn't proc
 
 @if we proc, set the offensive skill and poison flag
 ldr     r2,[r6]    
