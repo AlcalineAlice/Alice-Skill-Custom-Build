a4c21008c4f8d55a4c408f818f70e9a828a015ee
Alice <raulapablaza2001@gmail.com>
2025-07-12 00:29:05 -0400

    Reapply "Fix?"
    This reverts commit 90eed06c61d42004103c37bcea5cc22e208ebdad.

--- EngineHacks/SkillSystem/Skills/ProcSkills/Cancel/proc_cancel.s
+++ EngineHacks/SkillSystem/Skills/ProcSkills/Cancel/proc_cancel.s
@@ -19,18 +19,18 @@ mov r7, r3 @battle data
 ldr     r0,[r2]           @r0 = battle buffer                @ 0802B40A 6800     
 lsl     r0,r0,#0xD                @ 0802B40C 0340     
 lsr     r0,r0,#0xD        @Without damage data                @ 0802B40E 0B40     
-mov r1, #0xC0 @skill flag
-lsl r1, #8 @0xC000
-add r1, #2 @miss @@@@OR BRAVE??????
+@ mov r1, #0xC0 @skill flag
+@ lsl r1, #8 @0xC000
+mov r1, #2 @miss @@@@OR BRAVE??????
 tst r0, r1
 bne End
-@if another skill already activated, don't do anything
+//@if another skill already activated, don't do anything
 
-@check if we're already in astra
-ldrb r0, [r2, #4] @active skill
-@make sure no other skill is active
-cmp r0, #0
-bne End
+//@check if we're already in astra
+//ldrb r0, [r2, #4] @active skill
+//@make sure no other skill is active
+//cmp r0, #0
+//bne End
 
 @check if unit has CancelPlus, i.e. doesn't need to check for proc
 ldr r0, SkillTester
@@ -69,18 +69,18 @@ bne End
 ldrb r0, CancelID
 strb r0, [r6,#4] @save the skill ID at byte #4
 
-Proc:
-@if we proc, set the offensive skill for the attacker and the no counter flag for the defender
-ldr     r2,[r6]    
-lsl     r1,r2,#0xD                @ 0802B42C 0351     
-lsr     r1,r1,#0xD                @ 0802B42E 0B49     
-mov     r0, #0x40              @Set the flags
-lsl     r0, #8  
-orr     r1, r0
-ldr     r0,=#0xFFF80000                @ 0802B434 4804     
-and     r0,r2                @ 0802B436 4010     
-orr     r0,r1                @ 0802B438 4308     
-str     r0,[r6]                @ 0802B43A 6018
+//Proc:
+//@if we proc, set the offensive skill for the attacker and the no counter flag for the defender
+//ldr     r2,[r6]    
+//lsl     r1,r2,#0xD                @ 0802B42C 0351     
+//lsr     r1,r1,#0xD                @ 0802B42E 0B49     
+//mov     r0, #0x40              @Set the flags
+//lsl     r0, #8  
+//orr     r1, r0
+//ldr     r0,=#0xFFF80000                @ 0802B434 4804     
+//and     r0,r2                @ 0802B436 4010     
+//orr     r0,r1                @ 0802B438 4308     
+//str     r0,[r6]                @ 0802B43A 6018
 
 mov     r0, #0x20              @Make next round (defender's attack) not occur
 orr     r1, r0
