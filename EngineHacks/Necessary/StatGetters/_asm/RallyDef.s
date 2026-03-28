b3c595e27c655ab7dbdd151710ad9869a5fd6806
Alice <raulapablaza2001@gmail.com>
2026-03-28 19:16:28 -0400

    StaunchDef (WIP)

--- EngineHacks/Necessary/StatGetters/_asm/RallyDef.s
+++ EngineHacks/Necessary/StatGetters/_asm/RallyDef.s
@@ -37,6 +37,14 @@ beq noRallySpectrum
 add r4, #0x2
 noRallySpectrum:
 
+@ Staunch Defense
+mov r0, #0x40
+and r0, r1
+cmp r0, #0
+beq noStaunchDef
+add r4, #4
+noStaunchDef:
+
 End:
 mov r0, r4
 mov r1, r5
--- EngineHacks/SkillSystem/Skills/PostBattleSkills/StaunchSkills/StaunchDef.s
+++ EngineHacks/SkillSystem/Skills/PostBattleSkills/StaunchSkills/StaunchDef.s
@@ -6,18 +6,8 @@
   .short 0xF800
 .endm
 
-@ Layout of EALiterals (like Witch's Brew style):
-@ SkillTester:
-@   POIN SkillTester
-@   WORD StaunchDefID
-@   POIN GetDebuffs
-
-.equ SkillTesterPtr, SkillTester      @ points to POIN SkillTester
-.equ StaunchDefID, SkillTester+4      @ WORD StaunchDefID
-.equ GetDebuffsPtr, SkillTester+8     @ POIN GetDebuffs
-
-@ r4 = unit
-@ r6 = gActionData
+.equ StaunchDefID, SkillTester+4
+.equ GetDebuffsPtr, StaunchDefID+4
 
 StaunchDef:
   push {lr}
@@ -25,51 +15,49 @@ StaunchDef:
   @ if dead, do nothing
   ldrb r0, [r4, #0x13]
   cmp  r0, #0
-  beq  StaunchEnd
+  beq  End
 
-  @ check action == Wait (gActionData+0x11 == 1)
-  ldrb r0, [r6, #0x11]
-  cmp  r0, #0x01
-  bne  StaunchEnd
+  @ check if Wait was used
+  ldrb r0, [r6, #0x11]   @ action taken
+  cmp  r0, #1            @ Wait
+  bne  End
 
-  @ check same unit (allegiance byte)
+  @ same unit?
   ldrb r0, [r6, #0x0C]   @ acting unit allegiance
   ldrb r1, [r4, #0x0B]   @ this unit allegiance
   cmp  r0, r1
-  bne  StaunchEnd
+  bne  End
 
-  @ check for StaunchDef skill
+  @ has StaunchDef?
   mov  r0, r4
   ldr  r1, StaunchDefID
-  ldr  r3, SkillTesterPtr
-  ldr  r3, [r3]          @ load actual SkillTester pointer
+  ldr  r3, SkillTester     @ POIN SkillTester
   mov  lr, r3
   .short 0xF800
   cmp  r0, #0
-  beq  StaunchEnd
+  beq  End
 
-  @ get debuff struct for this unit
+  @ get debuff struct
   mov  r0, r4
-  ldr  r3, GetDebuffsPtr
-  ldr  r3, [r3]          @ load actual GetDebuffs pointer
+  ldr  r3, GetDebuffsPtr   @ POIN GetDebuffs
   mov  lr, r3
   .short 0xF800
-  mov  r3, r0            @ r3 = debuff pointer
+  mov  r3, r0              @ r3 = debuff pointer
 
-  @ set StaunchDef bit (0x40) in byte 3
+  @ set bit 0x40 in byte 3
   ldrb r0, [r3, #3]
-  mov  r1, #0x40         @ StaunchDef flag
+  mov  r1, #0x40
   orr  r0, r1
   strb r0, [r3, #3]
 
-StaunchEnd:
+End:
   pop {r0}
-  bx  r0
+  bx r0
 
+  .ltorg
   .align
 
 SkillTester:
-  @ EALiterals filled by EA:
   @ POIN SkillTester
   @ WORD StaunchDefID
   @ POIN GetDebuffs
