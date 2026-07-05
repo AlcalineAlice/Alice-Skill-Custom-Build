95dcecdc7ad0ad215006625baae196ef77daab1d
Alice <raulapablaza2001@gmail.com>
2026-07-05 14:38:30 -0400

    Reapply "Mug testing. Gives wrong item."
    This reverts commit 6115a59779f46d933ebe0190fb471c8fcfe3e4ea.

--- EngineHacks/SkillSystem/Skills/PreBattleSkills/Mug/mug_pre_battle.s
+++ EngineHacks/SkillSystem/Skills/PreBattleSkills/Mug/mug_pre_battle.s
@@ -1,6 +1,7 @@
 .thumb
 .equ MugPendingDropFlag, 0x03003F48
 .equ MugID, SkillTester+4
+.equ MemorySlot3, 0x030004E8   @ item slot (halfword)
 
 push {lr}
 
@@ -19,6 +20,11 @@ beq  EndPre
 mov  r0, #1
 ldr  r1, =MugPendingDropFlag
 strb r0, [r1]
+
+@ write Item ID 0x02 (Slim Sword) into memory slot 3
+ldr  r1, =MemorySlot3
+mov  r0, #0x02
+strh r0, [r1]
 
 EndPre:
 pop {r0}
