d626a5fa4941dc457b30df6059d4acb7497d4832
Alice <raulapablaza2001@gmail.com>
2026-07-05 20:34:57 -0400

    More fixes. Still gives wrong item

--- EngineHacks/SkillSystem/Skills/PostBattleSkills/Mug/mug_post_battle.s
+++ EngineHacks/SkillSystem/Skills/PostBattleSkills/Mug/mug_post_battle.s
@@ -20,7 +20,13 @@ cmp  r0, #1
 bne  EndPost
 
 @ ---------------------------------------------------------
-@ NEW: write MugItem into MemorySlot3 before running event
+@ NEW: clear MemorySlot3 BEFORE writing MugItem
+ldr  r2, =MemorySlot3
+mov  r0, #0
+strh r0, [r2]
+@ ---------------------------------------------------------
+
+@ write MugItem into MemorySlot3 before running event
 ldr  r1, =MugItem
 ldrh r0, [r1]          @ r0 = MugItem (Slim Sword = 0x02)
 ldr  r2, =MemorySlot3
@@ -36,7 +42,7 @@ mov r1, #0x01           @ wait for events
 .short 0xF800
 
 @ ---------------------------------------------------------
-@ NEW: clear entire MugMemory (flag + item)
+@ clear entire MugMemory (flag + item)
 ldr r1, =MugMemory
 mov r0, #0
 strb r0, [r1]          @ clear flag
